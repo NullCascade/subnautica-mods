e7592490102e6f3c5703910c01ec830b6dca210d
NullCascade <nullcascade@gmail.com>
2023-04-16 09:24:47 -0700

    Add Immersive Time Capsules.

---
+++ ImmersiveTimeCapsules/Patches/TimeCapsulePatches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+namespace ImmersiveTimeCapsules.Patches
+{
+    [HarmonyPatch(typeof(TimeCapsule))]
+    internal class TimeCapsulePatches
+    {
+        [HarmonyPatch(nameof(TimeCapsule.Start))]
+        [HarmonyPrefix]
+        public static bool Start_Prefix(TimeCapsule __instance)
+        {
+            __instance.spawned = false;
+            Plugin.Log.LogMessage($"Destroying time capsule at {__instance.gameObject.transform.position}");
+            UnityEngine.Object.Destroy(__instance.gameObject);
+            return false;
+        }
+    }
+}
---
+++ ImmersiveTimeCapsules/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ImmersiveTimeCapsules
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.NullCascade.ImmersiveTimeCapsules";
+        private const string PluginName = "Immersive Time Capsules";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake()
+        {
+            Log = Logger;
+            Harmony.PatchAll();
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+        }
+    }
+}
